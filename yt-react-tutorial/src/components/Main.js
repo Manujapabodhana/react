4fadaf7b79b5ab92606692fe62d476a93a688e0b
manuja <rpmanuja123@gmail.com>
2025-05-13 17:29:13 +0530

    Update Main.js

--- yt-react-tutorial/src/components/Main.js
+++ yt-react-tutorial/src/components/Main.js
@@ -1,16 +1,29 @@
-function Main(props){
-    return(
+const name = 'manuja';
+  const position = 'se';
+
+  class myObj ={
+    name:`${name}`,
+    position:`${position}`,
+  };
+
+
+function Main(props) {
+  
+
+  return (
     <div>
-        This is main components
-        <ul>
-            <li>dddd</li>
-            <li>dddssd</li>
-            <li>dsdd</li>
-            <li>ds</li>
-            {props.children}
-        </ul>
-        
-    </div>);
+      This is main components
+      <ul>
+        <li>{myObj.name}</li>
+        <li>{myObj.position}</li>
+        <li>dsdd</li>
+        <li>ds</li>
+
+        {/* 👇 This will render whatever is passed from <Main> in App.js */}
+        {props.children}
+      </ul>
+    </div>
+  );
 }
 
 export default Main;
